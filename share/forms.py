83bcc0afc5c31856798c7f435ed27fcd324a8836
Kisavi <deniskisavi@gmail.com>
2022-06-22 21:52:20 +0300

    create the post, comment and update profile forms from their respective models

--- share/forms.py
+++ share/forms.py
@@ -1,6 +1,32 @@
 from django import forms
-from .models import Profile
+from .models import Image, Profile, Comment
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('image', 'caption')
+
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment....'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
 
 
 class ProfileForm(forms.ModelForm):
