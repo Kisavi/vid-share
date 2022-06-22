3dd8dbacbd6838011508c08ed075416b7fcbb23f
Kisavi <deniskisavi@gmail.com>
2022-06-22 21:33:04 +0300

    update the views

--- share/views.py
+++ share/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Image, Profile, Follow, Comment
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponseRedirect
-from .forms import ProfileForm
+from .forms import UploadForm, ProfileForm, CommentForm, RegisterForm
+from django.urls.base import reverse
+from django.http.response import Http404
+
+
+# Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def index(request):
+    images = Image.images()
+    users = User.objects.exclude(id=request.user.id)
+    context = {
+        "users": users,
+        "images": images[::1]
+    }
+    return render(request, 'index.html', context)
 
 
 def profile(request, username):
@@ -22,20 +46,60 @@ def profile(request, username):
     }
     return render(request, 'main/profile.html', ctx)
 
-def profile(request, username):
-    images = request.user.profile.images.all()
+
+def post(request):
     if request.method == 'POST':
-        prof_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
-        if prof_form.is_valid():
-            prof_form.save()
+        form = UploadForm(request.POST, request.FILES)
+        print(form.errors)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return redirect('landing')
+    else:
+        form = UploadForm()
+    return render(request, 'post_image.html', {"form": form})
+
+
+def comment(request, id):
+    image = get_object_or_404(Image, pk=id)
+    comments = image.comment.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.photo = image
+            comment.user = request.user.profile
+            comment.save()
             return HttpResponseRedirect(request.path_info)
     else:
-        prof_form = ProfileForm(instance=request.user.profile)
+        form = CommentForm()
     context = {
-        'prof_form': prof_form,
-        'images': images,
+        'image': image,
+        'form': form,
+        'comments': comments,
     }
-    return render(request, 'profile.html', context)
+    return render(request, 'post.html', context)
+
+
+def like_post(request, id):
+    post = Image.objects.get(pk=id)
+    is_liked = False
+    user = request.user.profile
+    try:
+        profile = Profile.objects.get(user=user.user)
+        print(profile)
+
+    except Profile.DoesNotExist:
+        raise Http404()
+    if post.likes.filter(id=user.user.id).exists():
+        post.likes.remove(user.user)
+        is_liked = False
+    else:
+        post.likes.add(user.user)
+        is_liked = True
+    return HttpResponseRedirect(reverse('landing'))
+
 
 def profile(request, username):
     images = request.user.profile.images.all()
@@ -102,6 +166,10 @@ def user_profile(request, username):
         'follow_status': follow_status
     }
     return render(request, 'user_profile.html', context)
+
+
+
+
 
 
 
