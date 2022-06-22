64382d0a9093091b8108a78f2108ed7b5591ccdb
colletemine <jepngetichcolleta@gmail.com>
2022-06-22 18:52:10 +0300

    Add search profile and user profile functions

--- share/urls.py
+++ share/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     
     path('profile/<str:username>/', views.profile, name='profile'),
     path('edit/profile/', views.update_profile, name='update'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('search/', views.search_profile, name='search'),
   
 ]
 
--- share/views.py
+++ share/views.py
@@ -12,20 +12,6 @@ def profile(request, username):
     user = User.objects.get(username=current_user.username)
     user_select = User.objects.get(username=username)
     if user_select == user:
-        return redirect('profile', username=request.user.username)
-
-
-    ctx = {
-        
-        "profile": profile,
-        'user': user,
-    }
-    return render(request, 'main/profile.html', ctx)
-def profile(request, username):
-    current_user = request.user
-    user = User.objects.get(username=current_user.username)
-    user_select = User.objects.get(username=username)
-    if user_select == user:
         return redirect('instagram:profile', username=request.user.username)
 
     posts = Image.objects.filter(user=user_select.id)
@@ -79,5 +65,44 @@ def update_profile(request):
     else:
         form = ProfileForm()
     return render(request, 'edit_profile.html', {"form": form})
+
+
+def search_profile(request):
+    if 'search_user' in request.GET and request.GET['search_user']:
+        name = request.GET.get("search_user")
+        results = Profile.search_profile(name)
+        print(results)
+        message = name
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "You did not make a selection"
+    return render(request, 'results.html', {'message': message})
+
+
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    user_posts = user_prof.profile.images.all()
+
+    followers = Follow.objects.filter(followed=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.follower:
+            follow_status = True
+        else:
+            follow_status = False
+    context = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    return render(request, 'user_profile.html', context)
+
 
 
