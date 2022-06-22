8bcc4fd97beb3893fc4e5729ad49e59516b00b20
Kisavi <deniskisavi@gmail.com>
2022-06-22 21:42:17 +0300

    update the user profile and update profile urls

--- share/urls.py
+++ share/urls.py
@@ -7,6 +7,14 @@ urlpatterns = [
     path('edit/profile/', views.update_profile, name='update'),
     path('user_profile/<username>/', views.user_profile, name='user_profile'),
     path('search/', views.search_profile, name='search'),
+    path('home', views.index, name='landing'),
+    path('signup', views.sign_up),
+    path('', views.sign_up, name='index'),
+    path('image/', views.post, name='post'),
+    path('like/<id>', views.like_post, name='like'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+    path('image/<id>', views.comment, name='comment')
   
 ]
 
